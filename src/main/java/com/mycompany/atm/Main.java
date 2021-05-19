574418c3f0fba39df76f5748f0f5d8e312eb6ba2
Achmad Syarifudin Thomas <achmadsy@live.com>
2021-05-20 06:00:03 +0700

    - Update Main class for spring boot support

--- src/main/java/com/mycompany/atm/Main.java
+++ src/main/java/com/mycompany/atm/Main.java
@@ -8,12 +8,28 @@ package com.mycompany.atm;
 import com.mycompany.atm.service.MenuService;
 import com.mycompany.atm.service.TransactionService;
 import java.util.NoSuchElementException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  *
  * @author Achmad_ST761
  */
-public class Main {
+@SpringBootApplication
+@EntityScan
+@ComponentScan("com.mycompany.atm")
+public class Main implements ApplicationRunner {
+    
+    private static final Logger LOG = LoggerFactory.getLogger(Main.class);
+    
+    @Value("${filePath}")
     private static String filePath = "";
     
     public static void main(String[] args) {
@@ -22,12 +38,17 @@ public class Main {
         }
         TransactionService transactionService = new TransactionService(filePath);
         MenuService menuService = new MenuService(transactionService);
-        menuService.clearScreen();
+        
         try {
-            menuService.showWelcomeScreen();
+            SpringApplication.run(Main.class, args);
         } catch (NoSuchElementException e) {
             System.exit(0);
         }
+    }
+    
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        LOG.info("CSV filepath: "+filePath);
     }
     
 }
